478d881653918d0ef70b8841c6ffa02080c913fb
iNaeRoZ <naeroz@outlook.fr>
2024-01-18 18:56:12 +0100

    mise en place Q4 + modif

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import HeaderPage from "./components/HeaderPage";
-import SliderVertical from "./components/SliderVertical";
 
 function App() {
   return (
     <div className="App">
-      <HeaderPage page="01" totalPages="05" title="DEFINE YOUR HAIR TYPE" />
-      <SliderVertical />
+      <Outlet />
     </div>
   );
 }
--- frontend/src/pages/q4-conditions/Q4Conditions.jsx
+++ frontend/src/pages/q4-conditions/Q4Conditions.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Q4Conditions.css";
+import "../../components/Buttons.css";
+import Buttons from "../../components/Buttons";
+import HeaderPage from "../../components/HeaderPage";
 
 function Q4Conditions() {
-  return <div>Q4Conditions</div>;
+  const [conditions, setConditions] = useState([]);
+  const addCondition = (condition) => {
+    let newConditions = conditions;
+    if (newConditions.includes(condition)) {
+      newConditions = newConditions.filter((cond) => cond !== condition);
+    } else {
+      newConditions.push(condition);
+    }
+    setConditions(newConditions);
+  };
+
+  return (
+    <div>
+      <HeaderPage
+        page="04"
+        totalPages="05"
+        title="DEFINE YOUR HAIR CONDITIONS"
+      />
+      <div className="allButtons">
+        <Buttons type="Normal" addCondition={(value) => addCondition(value)} />
+        <Buttons
+          type="Greasy Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Curly Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Damaged Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Colored Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Thin Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Long Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Short Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+        <Buttons
+          type="Senstive Hair"
+          addCondition={(value) => addCondition(value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Q4Conditions;
