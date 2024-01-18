f97d7ed315fe5337db249825027d1e97c6163bc2
Grégory Duffort <g.duffort@gmail.com>
2024-01-18 15:39:03 +0100

    Page disclaimer

--- frontend/src/pages/disclaimer/Disclaimer.jsx
+++ frontend/src/pages/disclaimer/Disclaimer.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 import "./Disclaimer.css";
+import LogoP from "../../assets/logo.svg";
 
 function Disclaimer() {
-  return <div>Disclaimer</div>;
+  return (
+    <div className="login-page">
+      <img className="p-logo" src={LogoP} alt="logoP" />
+      <h1 className="login-title">
+        Before meeting <br /> your beauty counsellor
+      </h1>
+      <div className="dis-line" />
+      <p className="login-text">
+        Embark on a personalized beauty journey by sharing insights about your
+        hair type, skin concerns, and makeup preferences. <br />
+        Because you're worth it!
+        <br />
+      </p>
+    </div>
+  );
 }
 
 export default Disclaimer;
