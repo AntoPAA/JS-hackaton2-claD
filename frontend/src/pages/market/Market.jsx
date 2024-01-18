231991a088c02ade098e3233f8adb92b9dfb1d47
OlvMrn <olivier.morin91@gmail.com>
2024-01-19 00:20:11 +0100

    ajout page market + ajustements divers

--- frontend/src/pages/market/Market.jsx
+++ frontend/src/pages/market/Market.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import HeaderPage from "../../components/HeaderPage";
+
+import "./Market.css";
+import MarketContainer from "../../components/MarketContainer";
 
 function Market() {
-  return <div>Market</div>;
+  const profileBasedProducts = [
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp1.png",
+      name: "Shampoo 1",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp2.png",
+      name: "Shampoo 2",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp3.png",
+      name: "Conditioner 1",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp4.png",
+      name: "Conditioner 2",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp5.png",
+      name: "Hair mask 1",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp6.png",
+      name: "Hair mask 2",
+    },
+  ];
+
+  const othersBasedProcucts = [
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp7.png",
+      name: "Shampoo 1",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp8.png",
+      name: "Conditioner 1",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp1.png",
+      name: "Hair mask 2",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp2.png",
+      name: "Shampoo 3",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp3.png",
+      name: "Shampoo 2",
+    },
+    {
+      image: "https://antopaa.github.io/Publicimg/img/shamp4.png",
+      name: "Conditioner 2",
+    },
+  ];
+
+  return (
+    <div className="market-page">
+      <div className="promo">5€ offered from 25€ of purchase</div>
+      <HeaderPage title="Based on your results" />
+      <MarketContainer products={profileBasedProducts} />
+      <HeaderPage title="Based on users favorites" lineHidden />
+      <MarketContainer products={othersBasedProcucts} />
+    </div>
+  );
 }
 
 export default Market;
