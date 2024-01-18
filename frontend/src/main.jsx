ba371417ef0d998dfc14b4a27a700bffeef8ee28
Antonin Pasquier <antonin.pasquier45@gmail.com>
2024-01-18 12:03:31 +0100

    dave

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import HairColor from "./pages/HairColor";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/hair-color",
+    element: <HairColor />,
   },
 ]);
 
